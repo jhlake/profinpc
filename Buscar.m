40cc6d734189fc595305af61f73d52252cc8f724
Pedro Salazar <p-salazar12>
2018-05-24 20:05:44 -0500

    Revert "pipi"
    This reverts commit b91534606c991b72997527aaa1d66e1b4b686ebd.

--- Buscar.m
+++ Buscar.m
@@ -51,7 +51,6 @@ function Buscar_OpeningFcn(hObject, eventdata, handles, varargin)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 % varargin   command line arguments to Buscar (see VARARGIN)
-movegui('center');
 
 % Choose default command line output for Buscar
 handles.output = hObject;
@@ -293,6 +292,5 @@ end
 % end
 
 guidata(hObject, handles);
-close;
 
 
